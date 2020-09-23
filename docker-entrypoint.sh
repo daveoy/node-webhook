0fdf7ebceaa14120146399c961b8cbfbe78d6194
Robert Gornall <robert@themill.com>
2020-09-23 16:47:29 +0000

    Try adding a few more r10k init clones

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d_ws --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d_rb --puppetfile
@@ -6,6 +7,7 @@
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_rb --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment windows --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment baselight --puppetfile
+/usr/local/bin/r10k -c /r10k.yaml deploy environment servers --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment production --puppetfile
 echo "starting server"
 cd /app
