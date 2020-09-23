0dd602017d8a44bfce3a34a44f6ee6708930faba
Robert Gornall <robert@themill.com>
2020-09-23 16:50:12 +0000

    Remove unused/old 3d environments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d --puppetfile
-/usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d_ws --puppetfile
-/usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d_rb --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_ws --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_rb --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment windows --puppetfile
