e9d079215cb4b533ad52e83d5cd5457f7a182abb
Dave Young <davey@themill.com>
2021-04-15 15:39:26 -0500

    removing this as it doesn't work since the secrets are read only

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-chmod 700 /root/.ssh
-chmod 600 /root/.ssh/id_rsa
-chmod 644 /root/.ssh/id_rsa.pub
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_ws --puppetfile
