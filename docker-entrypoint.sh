26a7514a0250a864be892d85edb957833596724f
Dave Young <davey@themill.com>
2021-04-15 15:04:58 -0500

    adding some chmods to docker entrypoint here since this projection is just not working

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/id_rsa
+chmod 644 /root/.ssh/id_rsa.pub
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_ws --puppetfile
