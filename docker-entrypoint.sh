5fb0d45b4c888ff3d5125d4414b05f5fe3a7b724
Dave Young <davey@themill.com>
2022-01-14 16:23:09 -0600

    updates to pull tcs and remove some old envs holding up init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment mill3d --puppetfile
-/usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_ws --puppetfile
-/usr/local/bin/r10k -c /r10k.yaml deploy environment mill2d_rb --puppetfile
+/usr/local/bin/r10k -c /r10k.yaml deploy environment tcs --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment windows --puppetfile
-/usr/local/bin/r10k -c /r10k.yaml deploy environment baselight --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment servers --puppetfile
 /usr/local/bin/r10k -c /r10k.yaml deploy environment production --puppetfile
 echo "starting server"
